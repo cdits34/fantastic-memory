84c9dc799507c698543cade27998cb4067898af7
cdits34 <calebmiller519@gmail.com>
2025-09-12 23:03:18 -0700

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,3 @@
-<script type="module">
-  // Import the functions you need from the SDKs you need
   import { initializeApp } from "https://www.gstatic.com/firebasejs/12.2.1/firebase-app.js";
   import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.2.1/firebase-analytics.js";
   // TODO: Add SDKs for Firebase products that you want to use
